2ff6183e48fa0401134ec181572be3a0d50b5823
Robert de Bock <robert@meinit.nl>
2016-05-19 16:02:12 +0200

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
 backupdatabase() {
-  docker exec mysql sh -c 'exec mysqldump --all-databases -uroot -p"rundeck"' > data/database.mysql
+  echo -n "Backing up database: "
+  docker exec mysql sh -c 'exec mysqldump -uroot -p"rundeck" rundeck' > data/database.mysql 2> /dev/null
+  if [ $? = 0 ] ; then
+    echo "Okay."
+  else
+    echo "Failed."
+  fi
 }
 
 backupfiles() {
- for directory in /var/rundeck /var/lib/rundeck/var/storage /var/lib/rundeck/logs ; do 
-   filename=$(echo ${directory} | sed 's%/%%g').tar.gz
-   docker exec rundeck /bin/bash -c "cd ${directory} ; tar -cvzf - *" > data/${filename}
- done
+  echo -n "Backing up files: "
+  for directory in /var/rundeck /var/lib/rundeck/var/storage /var/lib/rundeck/logs ; do
+    filename=$(echo ${directory} | sed 's%/%%g').tar.gz
+    docker exec rundeck /bin/bash -c "cd ${directory} ; tar -czf - *" > data/${filename}
+  done
+  if [ $? = 0 ] ; then
+    echo "Okay."
+  else
+    echo "Failed."
+  fi
 }
 
 backupdatabase
