31f44a0f9396d966ef790976d10623a0cc292972
Robert de Bock <robert@meinit.nl>
2016-05-19 16:01:46 +0200

    Update restore.sh

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,15 +1,27 @@
-#!/bin/bash -x
+#!/bin/bash
 
 restoredatabase() {
-  docker exec -i mysql mysql -uroot -p"rundeck" < data/database.mysql
+  echo -n "Restoring database: "
+  docker exec -i mysql mysql -uroot -p"rundeck" rundeck < data/database.mysql > /dev/null 2>&1
+  if [ $? = 0 ] ; then
+    echo "Okay."
+  else
+    echo "Failed."
+  fi
 }
 
 restorefiles() {
+  echo -n "Restoring files: "
   for directory in /var/rundeck /var/lib/rundeck/var/storage /var/lib/rundeck/logs ; do
     filename=$(echo ${directory} | sed 's%/%%g').tar.gz
     cat data/${filename} | docker exec -i rundeck /bin/bash -c "cat > ${dirname}/${filename}"
-    docker exec rundeck tar -xvzf ${dirname}/${filename} -C ${directory}
+    docker exec rundeck tar -xzf ${dirname}/${filename} -C ${directory}
   done
+  if [ $? = 0 ] ; then
+    echo "Okay."
+  else
+    echo "Failed."
+  fi
 }
 
 restoredatabase
